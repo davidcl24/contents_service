ad06f2d60ffdc6ecd80abea7392e0ff23ea39af4
davidcl24 <davidcl.flor@gmail.com>
2025-06-05 22:25:56 +0200

    added genres router

---
+++ app/routers/genres.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+
+from app.schemas.genre import GenreResponse, GenreCreate, GenreUpdate
+from app.services.genre_service import GenreService
+
+router = APIRouter(tags=["Genres"])
+
+@router.post("/genres", response_model=GenreResponse)
+async def create_genre(genre: GenreCreate, service: GenreService = Depends()):
+    return service.create(genre)
+
+@router.get("/genres", response_model=list[GenreResponse])
+async def read_genres(service: GenreService = Depends()):
+    return service.get_all()
+
+@router.get("/genres/{id}", response_model=GenreResponse)
+async def read_genre(genre_id: int, service: GenreService = Depends()):
+    return service.get_by_id(genre_id)
+
+@router.patch("/genres/id", response_model=GenreResponse)
+async def update_genre(genre_id: int, genre_data: GenreUpdate, service: GenreService = Depends()):
+    return service.update(genre_id, genre_data)
+
+@router.delete("/genres/{id}", response_model=dict)
+async def delete_genre(genre_id: int, service: GenreService = Depends()):
+    return service.delete(genre_id)
